40aac1482ef6e579836c0cbe267115919cacf956
Giant77 <willyarsyad77@gmail.com>
2025-03-11 08:52:06 +0700

    feat: base routing for API, and sample database

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("tes")
+const http = require("http");
+
+const routes = {
+    "/": "Selamat datang di halaman utama!",
+    // "/about": "Ini adalah halaman Tentang Kami.",
+    // "/contact": "Hubungi kami di contact@website.com",
+    "/api/data": JSON.stringify({
+        message: "Hello, ini data API!",
+        status: "success",
+    }),
+};
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": req.url.startsWith("/api")
+            ? "application/json"
+            : "text/plain",
+    });
+    res.end(routes[req.url] || "404 - Halaman tidak ditemukan");
+});
+
+server.listen(3000, () => {
+    console.log("Server berjalan di http://localhost:3000");
+});
